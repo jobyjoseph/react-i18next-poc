49e8f2eadf766756e656b34aa667cdc93ab5e45d
Joby Joseph <reachjoby@gmail.com>
2024-05-17 10:55:12 -0700

    Workding draft

---
+++ src/locales/index.js
@@ -0,0 +1,12 @@
+import enUSResource from "./en_US.json";
+import frFRResource from "./fr_FR.json";
+const resources = {
+  en: {
+    translation: enUSResource,
+  },
+  fr: {
+    translation: frFRResource,
+  },
+};
+
+export default resources;
